93e698c65e68a8b3b72fb9b28d04717fcaceec12
sujane <sujane_mf@mail.ru>
2022-06-17 16:53:26 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson7SpringData/Lesson7SpringDataApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson7SpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson7SpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson7SpringDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/lesson7SpringData/Lesson7SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson7SpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson7SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
